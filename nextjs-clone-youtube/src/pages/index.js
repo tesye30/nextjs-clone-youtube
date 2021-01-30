cd45d8619752bdf8a200ba7634167afbe4b52a69
Lucas Couto <tesye30@hotmail.com>
2021-01-30 14:26:42 -0300

    Material Ui configuration

--- nextjs-clone-youtube/src/pages/index.js
+++ nextjs-clone-youtube/src/pages/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
 
 export default function Home() {
   return (
     <div>
       Clone youtube com Next.js + Material UI
+      <Button variant="outlined" color="secondary">
+        Primary
+      </Button>
+
     </div>
   );
 }
