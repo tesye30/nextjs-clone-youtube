40b0fce4d7117501079008d89ebb16dcc72cf322
Cout <tesye30@hotmail.com>
2021-03-16 19:26:36 -0300

    Config Multer S3

---
+++ nextjs-clone-youtube/src/utils/upload.js
@@ -0,0 +1,36 @@
+import aws from 'aws-sdk';
+import multer from 'multer';
+import multerS3 from 'multer-s3';
+import crypto from 'crypto';
+
+aws.config.update({
+  secretAccessKey: process.env.AWS_SECRET_KEY,
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+});
+
+const s3 = new aws.S3({ /* ... */ });
+
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: 'some-bucket',
+    ac1: 'public-read',
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    metadata: function (req, file, cb) {
+      cb(null, {fieldName: file.fieldname});
+    },
+    key: function (req, file, cb) {
+      crypto.randomBytes(16, (err, hash) => {
+        if (err) cb(err);
+
+        const fileName = `${hash.toString('hex')}-${file.originalname}`;
+
+        cb(null, fileName);
+      });
+      cb(null, Date.now().toString());
+    },
+  }),
+});
+
+export default upload;
